160e10c8eabd578fc93e479f5b51073d3f582a5f
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2025-09-07 22:22:52 -0700

    Created the skeleton for the file. Three classes, one for data structures, one for sorting algorithms, and one for searching algorithms. Also added the methods that will be completed in each class.

---
+++ Python/data_structures_and_algorithms.py
@@ -0,0 +1,215 @@
+class Record:
+
+    def __init__(self):
+        self.record = {}
+
+    def insert(self, key, value):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class Array:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+class LinkedList:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class BinaryTree:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class HashTable:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class Heap:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class Graph:
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return
+
+    def search(self):
+        return
+
+    def update(self):
+        return
+
+    def traverse(self):
+        return
+
+    def isEmpty(self):
+        return
+
+    def size(self):
+        return
+
+
+class SortingAlgorithms:
+
+    def bubble_sort(self):
+        return
+
+    def selection_sort(self):
+        return
+
+    def insertion_sort(self):
+        return
+
+    def merge_sort(self):
+        return
+
+    def quick_sort(self):
+        return
+
+    def heap_sort(self):
+        return
+
+    def counting_sort(self):
+        return
+
+    def radix_sort(self):
+        return
+
+    def bucket_sort(self):
+        return
+
+class SearchAlgorithms:
+
+    def linear_search(self):
+        return
+
+    def binary_search(self):
+        return
+
+    def hash_table_search(self):
+        return
+
+    def breadth_first_search(self):
+        return
+
+    def depth_first_search(self):
+        return
