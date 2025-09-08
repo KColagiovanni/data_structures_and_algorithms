9b2ba8c4745707d488e544ca961dab43d21fbfde
kcolagio <kcolagio@its.jnj.com>
2025-09-08 15:48:21 -0700

    Added a driver script

---
+++ Python/driver.py
@@ -0,0 +1,43 @@
+from data_structures_and_algorithms import Record
+
+record = Record()
+
+record.insert('name', "Kevin")
+record.insert('age', "41")
+record.insert('gender', "Male")
+
+record.print_record()
+
+record.insert('name', "Kevin")
+record.insert('age', "41")
+record.insert('gender', "Male")
+
+record.delete('age')
+
+record.print_record()
+
+record.insert('name', "Kevin")
+record.insert('age', "41")
+record.insert('gender', "Male")
+
+record.print_record()
+
+record.search('gender')
+
+record.update('age', '42')
+
+record.print_record()
+
+print(f'record_size is: {record.size()}')
+
+record.clear_record()
+
+record.clear_record()
+
+record.print_record()
+
+record.insert('name', "Kevin")
+record.insert('age', "41")
+record.insert('gender', "Male")
+
+record.print_record()
